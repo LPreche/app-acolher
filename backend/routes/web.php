95cc5a44af47869be71f5b2f49a1e4e8c18e018b
LPreche <paulorecheme@hotmail.com>
2026-09-02 20:13:49 -0300

    chore: remocao de arquivos nao utilizados e boilerplates

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'online',
+        'sistema' => 'Sistema Acolher API',
+        'versao' => '1.0.0',
+    ]);
 });
